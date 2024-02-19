ac6e6a7768b0c9e6a62c3f2feed4799a258268a0
Ravi Pandey <ravi2403143@gmail.com>
2024-02-19 14:50:44 +0530

    minor fix

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/layout/header";
-import GridDotBackground from "@/components/widgets/GridDotBackground";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
