69339ef330f38fa7ca2e02ac467e2c8a074e5204
Ravi Pandey <ravi2403143@gmail.com>
2024-02-27 12:26:33 +0530

    minor fixes

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -18,38 +18,37 @@ interface DeleteModalProps {
   children: React.ReactNode;
 }
 
-const DeleteModal = React.forwardRef<HTMLButtonElement, DeleteModalProps>(
-  ({ children, onDelete, data }, ref) => {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+const DeleteModal = ({ children, onDelete, data }: DeleteModalProps) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    return (
-      <>
-        <Tooltip color="danger" content="Delete">
-          <Button isIconOnly color="danger" variant="shadow" onPress={onOpen}>
-            <TrashIcon className="h-6 w-6" />
-          </Button>
-        </Tooltip>
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-          <ModalContent className="pt-2">
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex jus">{children}</ModalHeader>
-                <ModalFooter className="flex justify-center">
-                  <Button
-                    color="danger"
-                    variant="solid"
-                    onPress={() => onDelete(data)}
-                  >
-                    Delete
-                  </Button>
-                  <Button onPress={onClose}>Close</Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <Tooltip color="danger" content="Delete">
+        <Button isIconOnly color="danger" variant="shadow" onPress={onOpen}>
+          <TrashIcon className="h-6 w-6" />
+        </Button>
+      </Tooltip>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent className="pt-2">
+          {(onClose) => (
+            <>
+              <ModalHeader>{children}</ModalHeader>
+              <ModalFooter className="flex justify-center">
+                <Button
+                  color="danger"
+                  variant="solid"
+                  onPress={() => onDelete(data)}
+                >
+                  Delete
+                </Button>
+                <Button onPress={onClose}>Close</Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
 export default DeleteModal;
